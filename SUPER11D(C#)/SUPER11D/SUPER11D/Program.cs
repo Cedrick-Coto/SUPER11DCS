d688305c05c98fe8f0513e2f12d8bec42d13742e
Cedrick <cedrick.coto@gmail.com>
2026-02-27 14:29:12 -0600

    Ingreso de un comentario menor en el metodo principal

--- SUPER11D(C#)/SUPER11D/SUPER11D/Program.cs
+++ SUPER11D(C#)/SUPER11D/SUPER11D/Program.cs
@@ -8,6 +8,7 @@ namespace SUPER11D
         [STAThread]
         static void Main()
         {
+          //Inicialización de la application
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
